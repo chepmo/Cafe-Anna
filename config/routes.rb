953ea864cd0c13ab934417eada0b83f12b81692d
Morgane Le Deu <chep.mo@hotmail.fr>
2023-03-16 18:20:50 +0100

    home et routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/apropos', to: 'pages#apropos'
+  get '/noustrouver', to: 'pages#noustrouver'
 end
